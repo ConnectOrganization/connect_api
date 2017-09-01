ec6268237ffc209ffb41ca829070834286c27daa
anand <anand.lakshman@careerbuilder.com>
2017-09-01 15:45:24 +0530

    plot

---
+++ ConnectApi/ConnectApi.Tests/SampleTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ConnectApi.Tests
+{
+    public class SampleTests
+    {
+        [Fact(DisplayName = "Sample Test")]
+        public void SampleTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
